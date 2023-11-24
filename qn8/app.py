e6b5ae9c0c8ab5527517b1622f55993f27ade317
TadeiOba <127893053+TadeiOba@users.noreply.github.com>
2023-11-24 19:00:57 +0300

    Create a RESTful API using Flask to perform CRUD

--- qn8/app.py
+++ qn8/app.py
@@ -4,57 +4,52 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from flask import flash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
-class User(db.Model, UserMixin):
+# Book model
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
+
+# Routes
+
+@app.route('/')
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        new_user = User(username=username, password=password)
-        db.session.add(new_user)
+        title = request.form['title']
+        author = request.form['author']
+        new_book = Book(title=title, author=author)
+        db.session.add(new_book)
         db.session.commit()
-        flash('Registration successful! Please log in.', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html')
+        return redirect(url_for('index'))
+    return render_template('create.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    book = Book.query.get(id)
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.password == password:
-            login_user(user)
-            return redirect(url_for('dashboard'))
-        else:
-            flash('Invalid username or password. Please try again.', 'error')
-    return render_template('login.html')
+        book.title = request.form['title']
+        book.author = request.form['author']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', book=book)
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
+@app.route('/delete/<int:id>')
+def delete(id):
+    book = Book.query.get(id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     with app.app_context():
